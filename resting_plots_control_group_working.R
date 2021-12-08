850527d4d16224fadae4bf6e317a70d0bd707974
Jackie Ouzman <jackie.ouzman@csiro.au>
2021-12-08 16:36:57 +1030

    trying to get control groups into the resting plots

---
+++ resting_plots_control_group_working.R
@@ -0,0 +1,119 @@
+#step1 one pull out the all the resting data only.
+
+week1_2_3 <- read_csv("W:/VF/LongPlain/R_scripts_etc/VF_LP/resting_spots_for_paper/week1_2_3_start.csv", 
+                      col_types = cols(GMT = col_datetime(format = "%Y-%m-%d %H:%M:%S"), 
+                                       date = col_date(format = "%Y-%m-%d"), 
+                                       local_time = col_datetime(format = "%Y-%m-%d %H:%M:%S"), 
+                                       timeOfEvent = col_datetime(format = "%Y-%m-%d %H:%M:%S")))
+week1_2_3 <- week1_2_3 %>%
+  mutate(time = hms::as_hms(local_time))
+#only select the resting data
+week1_2_3_rest <- week1_2_3 %>%  filter(!is.na(week1_2_3$resting.))
+unique(week1_2_3_rest$group)
+
+week1_2_3_rest_control <- week1_2_3 %>%  filter(group == "control" )
+
+week1_2_3_rest_control <- week1_2_3_rest_control %>%
+  dplyr::select(
+    deviceName,
+    gpsData.lat,
+    gpsData.lng,
+    fencesID,
+    fencesID_1,
+    resting = resting.,
+    local_time,
+    date,
+    day_since_vf_start,
+    time
+  )
+# ## work with a small subset of data one animal one day.
+# rest_2020_10_21_9370004 <- week1_2_3_rest %>%
+#   filter(date == "2020-10-21" & deviceName == "9370004") %>% 
+#   arrange(local_time)
+
+#change the local time to correct format
+week1_2_3_rest_control$local_time <- as.POSIXct(week1_2_3_rest_control$local_time)
+
+
+#round the local time to the closest 10 mins
+
+week1_2_3_rest_control <- week1_2_3_rest_control %>%
+  mutate(hours = substr(time, 1, 2),
+         mins = substr(time, 4, 5),
+         round_local_time = lubridate::round_date(local_time, "10 minutes") )
+
+
+# create a df that has regular time step No sure I need this bit??
+min(week1_2_3_rest_control$round_local_time)
+max(week1_2_3_rest_control$round_local_time)
+
+time_step_df_control <- data.frame( round_local_time =
+                              seq(as.POSIXct(min(week1_2_3_rest$round_local_time)), #start time
+                                  as.POSIXct(max(week1_2_3_rest$round_local_time)), #end time
+                                  dminutes(10)) #inteval 
+)
+
+#my time step may not match the observered data how should I make it join?
+#when there are multiple entries for one rounded value I could:
+#1. take the unique value or take a average.
+# I will try taking a unique value and see what happens
+
+#only keep unique round time values for each animal
+week1_2_3_rest_dis_control <- week1_2_3_rest_control %>% 
+  group_by(deviceName) %>% 
+  distinct(round_local_time, .keep_all= TRUE)
+
+
+
+
+
+
+week1_2_3_rest_dis_control <- week1_2_3_rest_dis_control %>% 
+  dplyr::select(round_local_time, 
+                deviceName,
+                gpsData.lat,
+                gpsData.lng, 
+                resting,
+                fencesID_1,
+                day_since_vf_start)
+
+
+week1_2_3_rest_dis_control <- week1_2_3_rest_dis_control %>% 
+  mutate(resting_threshold = case_when(
+    resting >= 20 ~ 1,
+    TRUE ~ NA_real_))
+
+
+
+
+
+week1_2_3_rest_dis_control_sf <-
+  st_as_sf(week1_2_3_rest_dis_control,
+           coords = c("gpsData.lng", "gpsData.lat"),
+           crs = 4326,
+           agr = "constant")
+
+week1_2_3_rest_dis_control_sf_trans <-
+  st_transform(week1_2_3_rest_dis_control_sf, crs = 28354)
+
+week1_2_3_rest_dis_control_sf_trans_clip <-
+  st_intersection(week1_2_3_rest_dis_control_sf_trans, long_plains)    
+
+
+
+resting_location_VF_area1_control <- ggplot() +
+  geom_sf(data = long_plains, color = "black", fill = NA) +
+  geom_sf(data = filter(long_plains_Vf, day_since_trial_start == 1),
+          color = "black", fill = NA) +
+  geom_sf(data = filter(week1_2_3_rest_dis_control_sf_trans_clip,!is.na(resting_threshold)),
+          alpha = 0.01) +
+  theme_bw()+
+  theme(legend.position = "none",
+        axis.ticks = element_blank(), axis.text.x = element_blank(), axis.text.y = element_blank())+
+  labs(title= "VF area 1")
+
+
+
+resting_location_VF_area1_control
+
+
